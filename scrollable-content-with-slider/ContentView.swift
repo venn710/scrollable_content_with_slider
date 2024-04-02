f562f0f5429022e45ac0885864387ea28fe1edec
venn <vennboddula@gmail.com>
2024-04-02 21:22:25 +0530

    Initial Commit

---
+++ scrollable-content-with-slider/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  scrollable-content-with-slider
+//
+//  Created by Venkatesham Boddula on 02/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ scrollable-content-with-slider/scrollable_content_with_sliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrollable_content_with_sliderApp.swift
+//  scrollable-content-with-slider
+//
+//  Created by Venkatesham Boddula on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct scrollable_content_with_sliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
